20cfe43423786a71917ad215ad1a2cb5556fa008
kai <b10223052@yuntech.org.tw>
2020-01-10 17:36:20 +0800

    add Button component

---
+++ src/components/button/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import './style.scss';
+
+const TypeEnums = {
+	OK: 'ok',
+	CANCAL: 'cancel',
+};
+
+const {
+	OK
+} = TypeEnums;
+
+const propTypes = {
+	type: Object.values(TypeEnums),
+	children: PropTypes.string,
+	className: PropTypes.string,
+	onClick: PropTypes.func,
+};
+
+const defaultProps = {
+	type: OK,
+	onClick: () => {},
+};
+
+function Button({
+	type,
+	children,
+	className,
+	onClick,
+}) {
+	const icon = type === OK ? '+' : 'x' ;
+
+	return (
+		<button className={cx('button', className, `button--${type}`)} onClick={onClick}>
+			{icon}&nbsp;&nbsp;{children}
+		</button>
+	);
+}
+
+Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
+Button.TypeEnums = TypeEnums;
+
+export default Button;
