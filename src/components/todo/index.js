e760be9f53c8acb996deec131e38e8ac44ae0cd7
kai <b10223052@yuntech.org.tw>
2020-01-10 18:03:13 +0800

    add todo__title

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../button';
+import FontIcon from '../font-icon';
+import Checkbox from '../checkbox';
 import './style.scss';
 const propTypes = {};
 
 function Todo() {
 	return (
 		<div className="todo">
-			<div>title</div>
+			<div className="todo__title">
+				<div>
+					<Checkbox/>
+					<input className="todo__title-input" placeholder="Type Something Here…"></input>
+				</div>
+				<div>
+					<FontIcon type={FontIcon.IconTypeEnums.LINE_STAR}/>
+					<FontIcon
+						type={FontIcon.IconTypeEnums.PEN}
+						color={FontIcon.ColorEnums.LIGHT_BLUE}
+					/>
+				</div>
+			</div>
 			<div>content</div>
 			<div className="todo__footer">
 				<Button type={Button.TypeEnums.CANCAL}> Cancel </Button>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,4 @@
 import React, { useState, } from 'react';
-import reactIcon from '../../icon/react-icon.png';
 import Header from '../../components/header';
 import './style.scss';
 import Todo from '../../components/todo';
