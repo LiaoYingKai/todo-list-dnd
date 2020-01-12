ec6a41fb97c36a424bb0100412ef5604a021487b
kai <b10223052@yuntech.org.tw>
2020-01-12 22:03:51 +0800

    modify home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,14 +2,19 @@ import React, { useState, } from 'react';
 import Header from '../../components/header';
 import './style.scss';
 import Todo from '../../components/todo';
+import AddTodoBlock from '../../components/add-todo-black';
 
 function Home() {
+	const [isAdd, setIsAdd] = useState(false);
 
 	return (
 		<div className="home">
 			<Header/>
 			<div className="home__content">
-				<Todo/>
+				{isAdd ? 
+					<Todo onClickCancel={() => {setIsAdd(false);}}/> :
+					<AddTodoBlock onClick={() => {setIsAdd(true);}}/>
+				}
 			</div>
 		</div>
 	);
