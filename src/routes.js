7e5f257f4cc359e276d4b3dbc8e8cdb4ec145d0d
kai <b10223052@yuntech.org.tw>
2020-01-13 16:54:35 +0800

    delete no use page

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,13 @@
 import loadComponent from './route/loadable';
 
 const Home = loadComponent({ loader: () => import('./pages/Home') });
-const About = loadComponent({ loader: () => import('./pages/About') });
-const Contact = loadComponent({ loader: () => import('./pages/Contact') });
-const Me =  loadComponent({ loader: () => import('./pages/Contact/Me') });
-const Another =  loadComponent({ loader: () => import('./pages/Contact/Another') });
 
 export const routePaths = {
 	HOME: '/',
-	ABOUT: '/about',
-	CONTACT: '/contact',
-	CONTACT_ME: '/contact/me',
 };
 
 const {
 	HOME,
-	ABOUT,
-	CONTACT,
-	CONTACT_ME
 } = routePaths;
 
 const routes = [
@@ -25,25 +15,6 @@ const routes = [
 		path: HOME,
 		component: Home,
 		exact: true,
-	},
-	{
-		path: ABOUT,
-		component: About,
-	},
-	{
-		path: CONTACT,
-		component: Contact,
-		routes: [
-			{
-				path: CONTACT_ME,
-				component: Me,
-				exact: true,
-			},
-			{
-				path: '/contact/:another',
-				component: Another,
-			}
-		]
 	},
 ];
 
